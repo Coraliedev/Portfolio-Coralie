decbe9ddcea13a17459f8dabca760c7e49515163
CoralieDev <coralie.acquaert@gmail.com>
2023-08-23 09:49:04 +0200

    feat(navbar): mobile hambergur menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,10 @@
 import navdata from '../assets/data/nav.data.json';
+import { useState } from 'react';
 
 
 const NavBar = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const scrollToComponent = (id: string) => {
     const element = document.querySelector(id);
@@ -9,10 +12,18 @@ const NavBar = () => {
     element?.scrollIntoView({ behavior: "smooth" });
   };
 
-  return (
-    <nav className='flex justify-around items-center h-[17vh] '>
+  const toggleMenu = () => {
+    const menu = document.getElementById("menu-links");
+    if (menu) {
+      menu.classList.toggle("max-h-[300px]");
+    }
+    setMenuOpen(!menuOpen);
+  }
+
+  return (<>
+    <nav className='xl:flex justify-around items-center h-[17vh] hidden' id="desktop-nav">
       <div className='text-[2rem] cursor-default'>Coralie Acquaert</div>
-      <ul className='flex gap-[2rem] text-[1.5rem]'>
+      <ul className='gap-[2rem] text-[1.5rem] hidden xl:flex'>
         {navdata.sections.map((section) => (
           <li key={section.id}>
             <button className='hover:text-gray-400 hover:underline hover:underline-offset-[1rem] ' onClick={() => scrollToComponent(section.id)}>{section.name}</button>
@@ -20,6 +31,26 @@ const NavBar = () => {
         ))}
       </ul>
     </nav>
+    <nav id="hambergur-nav" className='flex justify-around items-center h-[17vh] xl:hidden'>
+      <div className='text-[2rem] cursor-default'>Coralie Acquaert</div>
+      <div className='relative inline-block'>
+        <div className='w-8 h-6 cursor-pointer flex flex-col justify-around' id="hamburger-icon" onClick={() => toggleMenu()}>
+          <span className={`block h-0.5 w-full bg-black transition-transform duration-300 ease-in-out ${menuOpen ? 'tranform rotate-45' : 'transform-none'}`}></span>
+          <span className={`block h-0.5 w-full bg-black transition-opacity duration-300 ease-in-out ${menuOpen ? 'opacity-0' : 'opacity-1'}`}></span>
+          <span className={`block h-0.5 w-full bg-black transition-transform duration-300 ease-in-out ${menuOpen ? 'transform -rotate-45' : 'transform-none'}`}></span>
+        </div>
+
+        <div id="menu-links" className='absolute flex flex-col right-0 top-full bg-white w-fit max-h-0 overflow-hidden transition-all duration-300 ease-in-ou'>
+          {navdata.sections.map((section) => (
+            <li key={section.id} className='list-none'>
+              <button className='block p-[10px] text-center text-2xl transition-all duration-300 ease-in-out w-full' onClick={() => { toggleMenu(); scrollToComponent(section.id) }}>{section.name}</button>
+            </li>
+          ))}
+        </div>
+      </div>
+
+    </nav>
+  </>
   )
 
 };
