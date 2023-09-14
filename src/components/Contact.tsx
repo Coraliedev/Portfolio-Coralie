2ceae0095e4b3efc7792f943301d5364c957bdd1
CoralieDev <coralie.acquaert@gmail.com>
2023-09-14 15:01:57 +0200

    style: responsive contact component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,16 +4,16 @@ import linkedin from '../assets/images/linkedin.png';
 
 const Contact: React.FC = () => {
   return (
-    <section id="contact" className='block relative section_padding my-0 mx-40 box-border min-h-fit flex justify-center flex-col h-[70vh] '>
-      <p className='text-center text-xl mb-4 text-gray-600 mt-4' >Get in Touch</p>
-      <h1 className='text-5xl text-center font-bold w-full mb-4'>Contact Me</h1>
-      <div id="contact-info-upper-container" className='flex justify-center rounded-full border border-gray-400 my-8 p-2 w-fit m-auto'>
-        <div id="contact-info-container" className='flex items-center justify-center gap-2 m-4'>
-          <img src={email} alt="email icon" id="icon contact-icon"  className='h-8 cursor-pointer' />
+    <section id="contact" className='block relative section_padding my-0 mx-0 box-border min-h-fit flex justify-center flex-col h-[40vh] lg:h-[70vh] w-full'>
+      <p className='text-center sm:text-xl text-lg mb-4 text-gray-600 mt-4 w-full' >Get in Touch</p>
+      <h1 className='sm:text-5xl text-3xl text-center font-bold w-full mb-4 w-full'>Contact Me</h1>
+      <div id="contact-info-upper-container" className='flex justify-center rounded-full border border-gray-400 sm:my-8 my-4 sm:p-2 p-1 sm:m-auto mr-2 ml-2 flex-wrap'>
+        <div id="contact-info-container" className='flex items-center justify-center gap-2 sm:m-4 m-2'>
+          <img src={email} alt="email icon" id="icon contact-icon"  className='h-8' />
           <p><a href="mailto:coralie.acquaert@gmail.com">coralie.acquaert@gmail.com</a></p>
        </div>
-       <div id="contact-info-container" className='flex items-center justify-center gap-2 m-4'>
-          <img src={linkedin} alt="linkedin icon" id="icon contact-icon"  className='h-8' />
+       <div id="contact-info-container" className='flex items-center justify-center gap-2 sm:m-4 m-2'>
+          <img src={linkedin} alt="linkedin icon" id="icon contact-icon"  className='h-6' />
           <p><a href="https://www.linkedin.com/in/coralie-acquaert/" target='_blank'>Linkedin</a></p>
        </div>
       </div>
